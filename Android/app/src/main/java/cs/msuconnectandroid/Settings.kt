0db8cb7cfaf1de2608e306f08d0dcbdec9da6342
Omar Martinez <omarti26@msudenver.edu>
2018-09-12 10:01:06 -0600

    adding the missing kotlin files

--- Android/app/src/main/java/cs/msuconnectandroid/Settings.kt
+++ Android/app/src/main/java/cs/msuconnectandroid/Settings.kt
@@ -6,16 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class Startup : Fragment() {
+class Settings : Fragment() {
 
     companion object {
-        fun newInstance() : Startup {
-            return Startup()
+        fun newInstance() : Settings {
+            return Settings()
         }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.activity_profile, container, false)
+        return inflater.inflate(R.layout.activity_settings, container, false)
     }
 
 }
