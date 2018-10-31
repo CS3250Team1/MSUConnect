d062a54951732d282c28e40cf0be1f0d9cab6076
kobmofo <rin.isobe@zoho.com>
2018-10-31 15:32:22 -0600

    add omar sample buildings on map

--- Android/app/src/main/java/cs/msuconnectandroid/MapsActivity.kt
+++ Android/app/src/main/java/cs/msuconnectandroid/MapsActivity.kt
@@ -18,10 +18,12 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import android.support.v4.app.ActivityCompat
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.location.Location
 import android.util.Log
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
+import com.google.android.gms.maps.model.PolygonOptions
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -203,11 +205,71 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+
+        val aheu = PolygonOptions()
+        aheu.fillColor(Color.RED)
+        aheu.clickable(true)
+        aheu.add(LatLng(39.743115, -105.006219))
+        aheu.add(LatLng(39.743922, -105.006919))
+        aheu.add(LatLng(39.744289, -105.005972))
+        aheu.add(LatLng(39.743434, -105.005331))
+        var aheuPL = googleMap!!.addPolygon(aheu)
+
+        val tivoli = PolygonOptions()
+        tivoli.fillColor(Color.RED)
+        tivoli.clickable(true)
+        tivoli.add(LatLng(39.74453774, -105.00595106))
+        tivoli.add(LatLng(39.74548642, -105.00667526))
+        tivoli.add(LatLng(39.74596076, -105.00569894))
+        tivoli.add(LatLng(39.74492959, -105.00493182))
+        var tivoliPL = googleMap!!.addPolygon(tivoli)
+
+        val msub = PolygonOptions()
+        msub.fillColor(Color.RED)
+        msub.clickable(true)
+        msub.add(LatLng(39.74350294, -105.00504351))
+        msub.add(LatLng(39.74373393, -105.00522322))
+        msub.add(LatLng(39.7437793, -105.00512398))
+        msub.add(LatLng(39.74408041, -105.00537879))
+        msub.add(LatLng(39.7440723, -105.00542902))
+        msub.add(LatLng(39.74409756, -105.00544981))
+        msub.add(LatLng(39.7441749, -105.00529357))
+        msub.add(LatLng(39.74438269, -105.00545868))
+        msub.add(LatLng(39.744591, -105.004995))
+        msub.add(LatLng(39.74445189, -105.00487509))
+        msub.add(LatLng(39.74440011, -105.00496103))
+        msub.add(LatLng(39.74434855, -105.00491946))
+        msub.add(LatLng(39.74442434, -105.00474981))
+        msub.add(LatLng(39.74412612, -105.00451861))
+        msub.add(LatLng(39.74387582, -105.00505081))
+        msub.add(LatLng(39.74359779, -105.00483881))
+        var msubPL = googleMap!!.addPolygon(msub)
+
+        val aero = PolygonOptions()
+        aero.fillColor(Color.RED)
+        aero.clickable(true)
+        aero.add(LatLng(39.744748, -105.008936))
+        aero.add(LatLng(39.744781, -105.008989))
+        aero.add(LatLng(39.744748, -105.009032))
+        aero.add(LatLng(39.744863, -105.009123))
+        aero.add(LatLng(39.744876, -105.009102))
+        aero.add(LatLng(39.744946, -105.009182)) //
+        aero.add(LatLng(39.744971, -105.009135))
+        aero.add(LatLng(39.744991, -105.009172))
+        aero.add(LatLng(39.745428, -105.008561)) //
+        aero.add(LatLng(39.745362, -105.008470))
+        aero.add(LatLng(39.745354, -105.008486))
+        aero.add(LatLng(39.745152, -105.008250))
+        aero.add(LatLng(39.745094, -105.008320))
+        aero.add(LatLng(39.745144, -105.008374))
+        var aeroPL = googleMap!!.addPolygon(aero)
+
         // Add a marker in Sydney and move the camera
         val msu = LatLng(39.743064, -105.006219)
         mMap.addMarker(MarkerOptions().position(msu).title("Marker in MSU Denver"))
         val zoomLevel = 16.0f
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(msu, zoomLevel))
         setUpMap()
+
     }
 }
